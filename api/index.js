f3d2bccb89a6d9393956042468a4f4f461c07c6d
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2023-11-02 00:02:05 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
-import express  from "express";
 // const express = require('express');
+import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+// connecting to mongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to DB...");
+}).catch((err) => {
+    console.log(err);
+});
 
 // listening the port
 app.listen(3000, () => {
