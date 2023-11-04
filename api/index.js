fac1d230395d38b7aae2a8e81fe643e9c1e0c20a
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2023-11-04 13:50:03 +0500

    created a sign up api route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import express  from "express";
 import mongoose from "mongoose";
 // importing router
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 // connecting to mongoDB
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to DB...");
@@ -22,3 +25,5 @@ app.listen(3000, () => {
 
 // Using user router
 app.use('/api/user', userRouter);
+
+app.use('/api/auth', authRouter);
