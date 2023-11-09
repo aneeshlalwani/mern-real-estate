593ab22cd2031b1b95481c114104ac78f426abd5
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2023-11-09 18:17:16 +0500

    middleware and funtion to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req, res) => {
+export const signup = async(req, res, next) => {
     const {username, email, password} = req.body;
+    // bcrypt is a node package, which is used to encrypting password in hash
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
     try{
         await newUser.save();
     res.status(201).json({message: "User created!"});
     }catch(error){
-        res.status(500).json(error.message);
+        next(errorHandler(550, 'error from the function'));
     }
     
 }
--- api/index.js
+++ api/index.js
@@ -27,3 +27,14 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 
 app.use('/api/auth', authRouter);
+
+// creating a middleware for handling possible errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
